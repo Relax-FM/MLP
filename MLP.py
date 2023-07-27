fix bugs, add some functionality to dataloader (add normalization)

MLP.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
     info_label_size = 4  # Кол-во столбцов с параметрами свечи
     correct_label_size = 1  # Кол-во столбцов с ответами в датасете
     start_position = 200  # Начальная позиция датасета (как бы с 200 позиции но по факту будет создавать для start_position+candle_count)
-    stop_position = 360  # Конечная позиция датасета (Правда конечная позиция. Создает датасет до stop_position позиции )
+    stop_position = 350  # Конечная позиция датасета (Правда конечная позиция. Создает датасет до stop_position позиции )
 
     candles_params_count = 3 # Кол-во столбцов с параметрами свечи
     additional_params_count = 1 # Дополнительный столбец с параметрами свечи
@@ -150,7 +150,7 @@ if __name__ == '__main__':
 
 
 
-    h = np.linspace(1, 2000, 2000)
+    h = np.linspace(1, 1500, 1500)
 
     fig, ax = plt.subplots(1, 1, figsize=(13, 9))
     ax.plot(h[:], losses[:])